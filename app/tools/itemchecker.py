dc81549b6c41132ad3256093eea2433a198fd9b0
tos <tos@mainmetall.de>
2022-02-01 18:11:13 +0100

    update item checker

--- app/tools/itemchecker.py
+++ app/tools/itemchecker.py
@@ -1,16 +1,16 @@
 class ItemChecker:
 
     @classmethod
-    def json_item(cls, d, k):
+    def json_item(cls, d, k, alt_return=None):
         try:
             return d[k]
         except Exception:
-            return None
+            return alt_return
 
     @classmethod
-    def json_item_from_array(cls, d, k, i):
+    def json_item_from_array(cls, d, k, i, alt_return=None):
         item = cls.json_item(d, k)
         try:
             return item[i]
         except Exception:
-            return None
+            return alt_return
