0dc4abb326e5ac708509a0676db898f2501b47b6
tos <tos@mainmetall.de>
2022-02-01 13:23:27 +0100

    update main

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import config as Conf
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = Conf.DB_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+Conf.DB.app = app
+Conf.DB.init_app(app)
+Conf.DB.create_all()
+
+if __name__ == "__main__":
+    app.run(host=Conf.APP_DEFAULT_HOST, port=Conf.APP_DEFAULT_PORT)
